5ab6eab9c5b300b328d135c0a92da9325bcc914b
Chris Junker <elevatemedfit@gmail.com>
2019-08-28 19:16:58 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
-class Song < ActiveRecord::Base
-
+class Song < ActiveRecord::Base#Song class inherits from AR::Base our macro methods.
+  belongs_to :artist
+  belongs_to :genre # Tells the Song class that it will produce objects that can belong to the artist and genre.
 end
